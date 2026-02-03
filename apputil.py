9d017b781c50b9a8c70f894f96f563b870d832f4
mosleym <mosleym@iu.edu>
2026-02-03 14:46:14 -0500

    finished sort_names() function

--- apputil.py
+++ apputil.py
@@ -40,9 +40,23 @@ def lowest_score(names, scores):
     return names[student_i]
 
 def sort_names(names, scores):
-    return None
+    """ Returns an array of student names sorted by their scores
+
+    Args:
+        names (np.array(str)): a list of student names
+        scores (np.array(int)): a list of scores corresponding to the names
+
+    Returns:
+        np.array(str): the sorted names
+    """
+
+    # Since we cannot combine data types in numpy arrays, we can find the
+    # sort order from the scores using argsort, then sort the names accordingly.
+    # To get descending order, we can reverse the array.
+    sort_order_array = np.argsort(scores)[::-1]
+    return names[sort_order_array]
 
 # temp testing
 names = np.array(['Bob', 'Alice', 'P', 'Q', 'R'])
-scores = np.array([5,1,4,2,3])
-print(lowest_score(names, scores))
+scores = np.array([1,5,2,4,3])
+print(sort_names(names, scores))
